033625a7ddb21246398c2393073a97a13882d876
Kamil So <sowicz09@gmail.com>
2023-06-06 20:04:38 +0200

    extend ts function, render function (need to make loop)

--- src/script.js
+++ src/script.js
@@ -26,9 +26,12 @@ var rowInputLine = rowInput === null || rowInput === void 0 ? void 0 : rowInput.
 var rowInputElement = rowInput === null || rowInput === void 0 ? void 0 : rowInput.querySelector('#line-element');
 var rowInputDescription = rowInput === null || rowInput === void 0 ? void 0 : rowInput.querySelector('#description');
 var rowInputDate = rowInput === null || rowInput === void 0 ? void 0 : rowInput.querySelector('#input-date');
+var rowInputStatus = rowInput === null || rowInput === void 0 ? void 0 : rowInput.querySelector('#sel-status');
 var rowInputName = rowInput === null || rowInput === void 0 ? void 0 : rowInput.querySelector('#input-name');
+var tableBody = document.getElementById('table-body');
 //NEW ROW ADD BUTTON
 var rowAddBtn = rowInput === null || rowInput === void 0 ? void 0 : rowInput.querySelector('.add-btn');
+// TEST TO CHECK INPUTS IF WORK WELL
 var show = function () {
     console.log(rowInputLine.value.replace(/-/g, " "));
     console.log(rowInputElement.value);
@@ -36,4 +39,33 @@ var show = function () {
     console.log(rowInputDate.value);
     console.log(rowInputName.value);
 };
-rowAddBtn === null || rowAddBtn === void 0 ? void 0 : rowAddBtn.addEventListener('click', show);
+// rowAddBtn?.addEventListener('click', show);
+// CLASS TO MAKE NEW ROW ON TABLE
+var Row = /** @class */ (function () {
+    function Row(count, line, element, descript, date, status, addby) {
+        this.count = count;
+        this.line = line;
+        this.element = element;
+        this.descript = descript;
+        this.date = date;
+        this.status = status;
+        this.addby = addby;
+    }
+    return Row;
+}());
+//ARRAY TO PUSH ROW DATA AS OBJ
+var rowTab = [];
+rowAddBtn === null || rowAddBtn === void 0 ? void 0 : rowAddBtn.addEventListener('click', getDataRow);
+function getDataRow(a) {
+    a = new Row(1, rowInputLine.value.replace(/-/g, " "), rowInputElement.value, rowInputDescription.value, rowInputDate.value, rowInputStatus.value, rowInputName.value);
+    rowTab.push(a);
+    console.log(rowTab);
+    renderData();
+}
+function renderData() {
+    var data = document.createElement('tr');
+    data.innerHTML =
+        "\n        <td>".concat(rowTab[0].count, "</td>\n        <td>").concat(rowTab[0].line, "</td>\n        <td>").concat(rowTab[0].element, "</td>\n        <td>").concat(rowTab[0].descript, "</td>\n        <td>").concat(rowTab[0].date, "</td>\n        <td><p class=\"").concat(rowTab[0].status, "\">").concat(rowTab[0].status, "</p></td>\n        <td>").concat(rowTab[0].addby, "</td>\n        <td><button class=\"edit-btn\">Edit</button></td>\n        <td><button class=\"del-btn\">Del</button></td>\n        ");
+    tableBody.append(data);
+    tableBody.insertBefore(data, tableBody.children[1]);
+}
--- src/script.ts
+++ src/script.ts
@@ -33,12 +33,15 @@ const rowInputLine = rowInput?.querySelector('#sel-line') as HTMLSelectElement;
 const rowInputElement = rowInput?.querySelector('#line-element') as HTMLSelectElement;
 const rowInputDescription = rowInput?.querySelector('#description') as HTMLSelectElement;
 const rowInputDate = rowInput?.querySelector('#input-date') as HTMLSelectElement;
+const rowInputStatus = rowInput?.querySelector('#sel-status') as HTMLSelectElement;
 const rowInputName = rowInput?.querySelector('#input-name') as HTMLSelectElement;
+const tableBody = document.getElementById('table-body') as HTMLSelectElement;
 
 //NEW ROW ADD BUTTON
 const rowAddBtn = rowInput?.querySelector('.add-btn');
 
 
+// TEST TO CHECK INPUTS IF WORK WELL
 const show = () => {    
     console.log(rowInputLine.value.replace(/-/g, " "))
     console.log(rowInputElement.value)
@@ -46,4 +49,73 @@ const show = () => {
     console.log(rowInputDate.value)
     console.log(rowInputName.value)
 }
-rowAddBtn?.addEventListener('click', show);
+// rowAddBtn?.addEventListener('click', show);
+
+
+
+// CLASS TO MAKE NEW ROW ON TABLE
+class Row {
+    count: number;
+    line: string;
+    element: string;
+    descript: string;
+    date: string;
+    status: string;
+    addby: string;
+
+    constructor(
+        count: number,
+        line: string,
+        element: string,
+        descript: string,
+        date: string,
+        status: string,
+        addby: string,
+        ) {
+            this.count = count;
+            this.line = line;
+            this.element = element;
+            this.descript = descript;
+            this.date = date;
+            this.status = status;
+            this.addby = addby;
+        }
+}
+
+//ARRAY TO PUSH ROW DATA AS OBJ
+let rowTab: any[] = [];
+
+rowAddBtn?.addEventListener('click', getDataRow);
+
+function getDataRow(a: object) {
+    a = new Row(
+        1,
+        rowInputLine.value.replace(/-/g, " "),
+        rowInputElement.value,
+        rowInputDescription.value,
+        rowInputDate.value,
+        rowInputStatus.value,
+        rowInputName.value,
+        )
+    rowTab.push(a);
+    console.log(rowTab);
+    renderData();
+}
+
+function renderData() {
+    const data = document.createElement('tr');
+    data.innerHTML = 
+        `
+        <td>${rowTab[0].count}</td>
+        <td>${rowTab[0].line}</td>
+        <td>${rowTab[0].element}</td>
+        <td>${rowTab[0].descript}</td>
+        <td>${rowTab[0].date}</td>
+        <td><p class="${rowTab[0].status}">${rowTab[0].status}</p></td>
+        <td>${rowTab[0].addby}</td>
+        <td><button class="edit-btn">Edit</button></td>
+        <td><button class="del-btn">Del</button></td>
+        `;
+    tableBody.append(data)
+    tableBody.insertBefore(data, tableBody.children[1]);
+}
